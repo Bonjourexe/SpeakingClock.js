639c4f7ff090cc1f221c8544cef569b516445f36
Bonjourexe <31899191+Bonjourexe@users.noreply.github.com>
2023-05-02 01:01:52 +0200

    Initial commit
    Added the SpeakingClock class

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SpeakingClock.js
-A JavaScript library to add a configurable speaking clock to a webpage
+A JavaScript library to add a configurable speaking clock to a webpage.
+
+## Installation
+Simply add the script to your page using a `script` tag:
+```html
+<script src="speakingclock.js"></script>
+```
+
+## Usage
+To create a speaking clock, create a new instance of the `SpeakingClock` class and pass any optional configuration options as an object:
+```JS
+const myClock = new SpeakingClock({
+  voice: myVoice, // Optional SpeechSynthesisVoice object
+  volume: 0.8, // Optional volume setting between 0 and 1 (default: 1)
+  pitch: 1.2, // Optional pitch setting between 0 and 2 (default: 1)
+  rate: 1.5, // Optional rate setting between 0.1 and 10 (default: 1)
+  lang: 'en-US' // Optional BCP 47 language tag (default: browser language)
+});
+```
+
+Once you have created a SpeakingClock instance, it will start announcing the time every minutes, starting one minute after creation.
+
+You can make an instance tell the time at any moment by calling its method `tellTime()`, or mute and unmute it using its methods `mute()` and `unmute()`.
---
+++ speakingclock.js
@@ -0,0 +1,61 @@
+/**
+ * Defines time units in milliseconds
+ */
+const UNITS = Object.freeze({
+    "SECOND": 1000,
+    "MINUTE": 1000*60,
+    "HOUR": 1000*60*60,
+    "DAY": 1000*60*60*24
+});
+
+/**
+ * A class that announces the time at regular intervals
+ * @constructor
+ * @param {Object} options Optional configuration for the speaking clock.
+ * @param {SpeechSynthesisVoice} options.voice The voice to use for speech synthesis.
+ * @param {number} options.volume The volume to use for speech synthesis. Must be between 0 and 1.
+ * @param {number} options.pitch The pitch to use for speech synthesis. Must be between 0 and 2.
+ * @param {number} options.rate The rate to use for speech synthesis. Must be between 0.1 and 10.
+ * @param {string} options.lang The language to use for speech synthesis. Must be a valid BCP 47 language tag. If not provided, defaults to the language of the user's browser.
+ */
+function SpeakingClock(options) {
+    options = Object.assign({
+        voice: null,
+        volume: 1,
+        pitch: 1,
+        rate: 1,
+        lang: navigator.language
+    }, options);
+    
+    let utterance = new SpeechSynthesisUtterance();
+    utterance.voice = options.voice;
+    utterance.volume = options.volume;
+    utterance.pitch = options.pitch;
+    utterance.rate = options.rate;
+    utterance.lang = options.lang;
+    
+    /**
+     * Gets the current time and utters in the instance's language
+     */
+    this.tellTime = () => {
+        utterance.text = new Date().toLocaleString(options.lang);
+        console.log(utterance);
+        window.speechSynthesis.speak(utterance);
+    }
+    
+    let interval = setInterval(this.tellTime, UNITS.MINUTE);
+
+    /**
+     * Sets the voice's volume to zero
+     */
+    this.mute = () => {
+        utterance.volume = 0;
+    }
+
+    /**
+     * Sets the voice's volume back to the instance's volume setting
+     */
+    this.unmute = () => {
+        utterance.volume = options.volume;
+    }
+}
