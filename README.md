3b7fc18462f212e9fcb02fd5151a181719837f8b
Bonjourexe <31899191+Bonjourexe@users.noreply.github.com>
2023-05-02 22:10:49 +0200

    Added interval option

--- README.md
+++ README.md
@@ -11,13 +11,28 @@ Simply add the script to your page using a `script` tag:
 To create a speaking clock, create a new instance of the `SpeakingClock` class and pass any optional configuration options as an object:
 ```JS
 const myClock = new SpeakingClock({
-  voice: myVoice, // Optional SpeechSynthesisVoice object
-  volume: 0.8, // Optional volume setting between 0 and 1 (default: 1)
-  pitch: 1.2, // Optional pitch setting between 0 and 2 (default: 1)
-  rate: 1.5, // Optional rate setting between 0.1 and 10 (default: 1)
-  lang: 'en-US' // Optional BCP 47 language tag (default: browser language)
+    voice: myVoice, // Optional SpeechSynthesisVoice object
+    volume: 0.8, // Optional volume setting between 0 and 1 (default: 1)
+    pitch: 1.2, // Optional pitch setting between 0 and 2 (default: 1)
+    rate: 1.5, // Optional rate setting between 0.1 and 10 (default: 1)
+    lang: 'en-US', // Optional BCP 47 language tag (default: browser language)
+    interval: 30000 // Optional interval in milliseconds (default: 60000)
 });
 ```
+
+>### Tip :
+>Time measurements can be represented using the `UNITS` object, which defines time units in milliseconds. The available units are:
+>- `UNITS.SECOND` (1000 milliseconds)
+>- `UNITS.MINUTE` (60000 milliseconds)
+>- `UNITS.HOUR` (3600000 milliseconds)
+>- `UNITS.DAY` (86400000 milliseconds)
+>
+>For example, to set the interval to 30 seconds, you would use:
+>```JS
+>const myClock = new SpeakingClock({
+>  interval: UNITS.SECOND * 30,
+>});
+>```
 
 Once you have created a SpeakingClock instance, it will start announcing the time every minutes, starting one minute after creation.
 
--- speakingclock.js
+++ speakingclock.js
@@ -3,13 +3,13 @@
  */
 const UNITS = Object.freeze({
     "SECOND": 1000,
-    "MINUTE": 1000*60,
-    "HOUR": 1000*60*60,
-    "DAY": 1000*60*60*24
+    "MINUTE": 1000 * 60,
+    "HOUR": 1000 * 60 * 60,
+    "DAY": 1000 * 60 * 60 * 24
 });
 
 /**
- * A class that announces the time at regular intervals
+ * A class that announces the time at regular intervals using the SpeechSynthesis API
  * @constructor
  * @param {Object} options Optional configuration for the speaking clock.
  * @param {SpeechSynthesisVoice} options.voice The voice to use for speech synthesis.
@@ -17,6 +17,7 @@ const UNITS = Object.freeze({
  * @param {number} options.pitch The pitch to use for speech synthesis. Must be between 0 and 2.
  * @param {number} options.rate The rate to use for speech synthesis. Must be between 0.1 and 10.
  * @param {string} options.lang The language to use for speech synthesis. Must be a valid BCP 47 language tag. If not provided, defaults to the language of the user's browser.
+ * @param {number} options.interval The time interval between announcements, in milliseconds. Defaults to 1 minute.
  */
 function SpeakingClock(options) {
     options = Object.assign({
@@ -24,16 +25,17 @@ function SpeakingClock(options) {
         volume: 1,
         pitch: 1,
         rate: 1,
-        lang: navigator.language
+        lang: navigator.language,
+        interval: UNITS.MINUTE
     }, options);
-    
+
     let utterance = new SpeechSynthesisUtterance();
     utterance.voice = options.voice;
     utterance.volume = options.volume;
     utterance.pitch = options.pitch;
     utterance.rate = options.rate;
     utterance.lang = options.lang;
-    
+
     /**
      * Gets the current time and utters in the instance's language
      */
@@ -42,8 +44,8 @@ function SpeakingClock(options) {
         console.log(utterance);
         window.speechSynthesis.speak(utterance);
     }
-    
-    let interval = setInterval(this.tellTime, UNITS.MINUTE);
+
+    let interval = setInterval(this.tellTime, options.interval);
 
     /**
      * Sets the voice's volume to zero
